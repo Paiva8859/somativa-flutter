5d14a3623d7a6b0f71da147108538c1fdd14d5bd
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2024-10-24 14:53:07 -0300

    a

--- somativa_flutter/lib/main.dart
+++ somativa_flutter/lib/main.dart
@@ -6,7 +6,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
-}
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
